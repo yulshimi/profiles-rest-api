2436c899a03e33ea5bed9bc51c911a034f344294
Phillip Jo <yulshimi4@gmail.com>
2020-08-09 16:58:50 -0700

    Hello API Post Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+        serializer_class = serializers.HelloSerializer #Define a form
+        def post(self, request):
+            serializer = self.serializer_class(data=request.data)
+            if serializer.is_valid():
+                name = serializer.validated_data.get('name')
+                message = f'Hello {name}'
+                return Response({'message': message})
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
         def get(self, request, format=None):
             an_apiview = [
                 'Uses API View',
